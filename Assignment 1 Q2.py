ebae36ae14563ec48f98ea37ab99479081265d82
Mishal Khalid <itsmishalk@gmail.com>
2025-11-23 00:08:30 +0500

    length of list

---
+++ Assignment 1 Q2.py
@@ -0,0 +1,5 @@
+arr = [324, 5, 2, 2]
+print(len(arr))
+print("The length of this array is "+str(len(arr)))
+print(f"The length of this new array is {len(arr)}")
+print("The length of this new new array is {}".format(len(arr)))
