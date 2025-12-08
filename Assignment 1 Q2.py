0a814343fd30e5adc1b2a8bfeaccac822a4e8fff
Mishal Khalid <itsmishalk@gmail.com>
2025-12-08 23:26:10 +0500

    Delete Assignment 1 Q2.py

--- Assignment 1 Q2.py
+++
@@ -1,5 +0,0 @@
-arr = [324, 5, 2, 2]
-print(len(arr))
-print("The length of this array is "+str(len(arr)))
-print(f"The length of this new array is {len(arr)}")
-print("The length of this new new array is {}".format(len(arr)))
