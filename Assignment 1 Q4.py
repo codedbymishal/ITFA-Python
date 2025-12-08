aa807c1d4b9682438e60ac3f386f1109538a63ad
Mishal Khalid <itsmishalk@gmail.com>
2025-12-08 23:26:32 +0500

    Delete Assignment 1 Q4.py

--- Assignment 1 Q4.py
+++
@@ -1,7 +0,0 @@
-str=input("Enter a sentence: ")
-if "badminton" in str:
-    print("This sentence contains the word badminton")
-else:
-    print("This sentence does not contain the word badminton")
-
-
