34bc592e16161eb7a0626a8fe08ebdd536f3a282
Mishal Khalid <itsmishalk@gmail.com>
2025-12-08 23:26:41 +0500

    Delete Assignment 1 Q5.py

--- Assignment 1 Q5.py
+++
@@ -1,6 +0,0 @@
-num1=float(input("Enter a float number: "))
-num2=float(input("Enter a float number: "))
-num3=float(input("Enter a float  number: "))
-sum=num1+num2+num3
-avg=sum/3
-print(f"Average of {num1}, {num2}, {num3} is {avg}")
