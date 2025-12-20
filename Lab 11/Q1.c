b3002adfdb3f20cf3574fcf8efac206b6cdaede7
Mishal Khalid <itsmishalk@gmail.com>
2025-12-20 22:58:05 +0500

    Calculate

--- Lab 11/Q1.c
+++ Lab 11/Q1.c
@@ -1,6 +1,7 @@
 num1=int(input("Enter num 1: "))
 num2=int(input("Enter num 2: "))
 op=int(input("Enter operator (1/2/3): "))
+    
 if op==1:
     print(f"The sum of {num1} and {num2} is {num1+num2}")
 elif op==2:
