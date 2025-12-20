f5dc4893e151eb13c031b354fc11fedba626a5a5
Mishal Khalid <itsmishalk@gmail.com>
2025-12-20 23:01:57 +0500

    Word at index

---
+++ Lab 11/Q6.c
@@ -0,0 +1,3 @@
+fruits=["apple","mango","banana","orange","peach","pineapple","strawberry"]
+index=int(input("Enter an index (0-5) to display a fruit!: "))
+print(f"fruit at index {index} is {fruits[index]}")
