909f692589b5bf459054a96e5bb55501d7cd3b48
Mishal Khalid <itsmishalk@gmail.com>
2025-12-20 23:02:39 +0500

    Smallest number

---
+++ Lab 11/Q7.c
@@ -0,0 +1,16 @@
+num1=int(input("Enter num 1: "))
+num2=int(input("Enter num 2: "))
+num3=int(input("Enter num 3: "))
+if num1<num2:
+    if num1<num3:
+        print(f"{num1} is the smallest")
+    else:
+        print(f"{num3} is the smallest")
+else:
+    if num2<num3:
+        print(f"{num2} is the smallest")
+    else:
+        print(f"{num3}is the smallest")
+
+        
+    
